52c8c86b007732c85cc037216fc567b56137766c
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-11-19 15:53:29 +0100

    new katas solved

---
+++ 6kyu/Detect_Pangram.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * A pangram is a sentence that contains every single letter of the alphabet at least once. 
+ * For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+ * because it uses the letters A-Z at least once (case is irrelevant).
+ * 
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace codeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"));
+        }
+        public static bool IsPangram(string str)
+        {
+            var k = str.ToLower().ToHashSet();
+            List<char> strabc = new List<char>() {'a','b','c','d','e','f','g','h','i',
+                'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+
+            int count = 0;
+
+            for (int i = 0; i < strabc.Count; i++)
+            {
+                // If not duplicate, add to result.
+                if (k.Contains(strabc[i]))
+                {
+                    count++;
+                }
+            }
+            return count >= 26 ? true : false;
+
+        }
+
+    }
+}
---
+++ 7kyu/Two_to_one.cs
@@ -0,0 +1,26 @@
+﻿/*
+  Take 2 strings s1 and s2 including only letters from a to z. 
+  Return a new sorted string, the longest possible, 
+  containing distinct letters - each taken only once - coming from s1 or s2.
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+ */
+using System.Linq;
+
+public class TwoToOne
+{
+
+    public static string Longest(string s1, string s2)
+    {
+        var hs1 = s1.ToHashSet();
+        var hs2 = s2.ToHashSet();
+        hs1.UnionWith(hs2);
+        var lista = hs1.ToList();
+        lista.Sort();
+        return string.Join("", lista);
+    }
+}
