df4149b44a820c2ed6cf06dc229ddeb40eca3d52
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-11-22 20:13:42 +0100

    new katas solved

---
+++ 7kyu/Credit_card_mask.cs
@@ -0,0 +1,29 @@
+﻿/*Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. 
+ * However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+Examples
+
+"4556364607935616" --> "############5616"
+*/
+
+public static class Kata
+{
+    // return masked string
+    public static string Maskify(string cc)
+    {
+        var fourChar = "";
+        string hastTag = "";
+
+        if (cc.Length <= 4)
+        {
+            return cc;
+        }
+        for (int i = 0; i < cc.Length - 4; i++)
+        {
+            fourChar = cc.Substring(cc.Length - 4);
+            hastTag = hastTag + "#";
+        }
+        return hastTag + fourChar;
+    }
+}
---
+++ 7kyu/Square_Every_Digit.cs
@@ -0,0 +1,24 @@
+﻿/* Description Kata:
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out,
+because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+ */
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int SquareDigits(int n)
+    {
+        var a = n.ToString().Select(x => int.Parse(x.ToString()));
+        string result = "";
+        foreach (var item in a)
+        {
+            result += (item * item);
+        }
+        return int.Parse(result);
+    }
+}
