b5ae1236abe12668465763482c545e24e3082884
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-11-23 21:32:42 +0100

    new kata solved

---
+++ 6kyu/Stop_gninnipS_My_sdroW!.cs
@@ -0,0 +1,41 @@
+﻿/* Description Kata:
+ * Write a function that takes in a string of one or more words, 
+ * and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). 
+ * Strings passed in will consist of only letters and spaces. 
+ * Spaces will be included only when more than one word is present.
+
+Examples:
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test"
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static string SpinWords(string sentence)
+    {
+        var annan = "";
+        var result = "";
+        string[] words = sentence.Split(' ');
+
+        foreach (var word in words)
+        {
+            if (word.Count() > 4)
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    annan += word[i];
+                }
+                result += ' ' + annan;
+                annan = "";
+            }
+            else
+                result += ' ' + word;
+
+        }
+        return result.TrimStart();
+    }
+}
