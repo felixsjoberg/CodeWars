16de4805be12d6b52faa6ee6abda53ec7a2eb931
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-11-24 11:43:39 +0100

    new katas solved

---
+++ 6kyu/Duplicate_Encoder.cs
@@ -0,0 +1,35 @@
+﻿/* Description Kata:
+ * The goal of this exercise is to convert a string to a new string where each character in the new string is 
+ * "(" if that character appears only once in the original string, or ")" 
+ * if that character appears more than once in the original string. 
+ * Ignore capitalization when determining if a character is a duplicate.
+Examples:
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+*/
+
+using System.Linq;
+public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        word = word.ToLower();
+        var result = "";
+        foreach (var ord in word)
+        {
+            var occurrence = word.Count(ord.ToString().Contains);
+            if (occurrence > 1)
+            {
+                result += ")";
+            }
+            else
+            {
+                result += "(";
+            }
+        }
+        return result;
+    }
+}
---
+++ 6kyu/Find_The_Parity_Outlier.cs
@@ -0,0 +1,28 @@
+﻿/* Description Kata:
+ * You are given an array (which will have a length of at least 3, but could be very large) 
+ * containing integers. 
+ * The array is either entirely comprised of odd integers or 
+ * entirely comprised of even integers except for a single integer N. 
+ * Write a method that takes the array as an argument and returns this "outlier" N.
+Examples:
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+
+*/
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        var even = integers.Where(x => x % 2 == 0);
+        var odd = integers.Where(x => x % 2 != 0);
+        return even.Count() > 1 ? int.Parse(string.Join("", odd)) : int.Parse(string.Join("", even));
+    }
+}
